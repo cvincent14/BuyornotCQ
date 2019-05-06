7d649c9a3c4b5420039ba16580b2c4b2eb541c74
Qlecap <lecapitaine.quentin@gmail.com>
2019-05-06 19:08:00 +0200

    class Additif

--- app/src/main/java/com/example/slam/buyornot/Additif.java
+++ app/src/main/java/com/example/slam/buyornot/Additif.java
@@ -1,10 +1,15 @@
 package com.example.slam.buyornot;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 
 public class Additif {
     private int id;
-    private String produit;
+    private String fonction;
+    private String description;
+    private String origine;
+    private String risques;
+    private ArrayList<Additif> lesAdditifs;
     private ArrayList<Produit> lesProduits;
 
     public Additif() {
@@ -12,7 +17,6 @@ public class Additif {
 
     public Additif(int id, String produit) {
         this.id = id;
-        this.produit = produit;
     }
 
     public int getId() {
@@ -23,12 +27,51 @@ public class Additif {
         this.id = id;
     }
 
-    public String getProduit() {
-        return produit;
+    public String getFonction() {
+        return fonction;
     }
 
-    public void setProduit(String produit) {
-        this.produit = produit;
+    public void setFonction(String fonction) {
+        this.fonction = fonction;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrigine() {
+        return origine;
+    }
+
+    public void setOrigine(String origine) {
+        this.origine = origine;
+    }
+
+    public String getRisques() {
+        return risques;
+    }
+
+    public void setRisques(String risques) {
+        this.risques = risques;
+    }
+
+    public ArrayList<Additif> getContient() {
+        return lesAdditifs;
+    }
+
+    public void setContient(ArrayList<Additif> contient) {
+        lesAdditifs = contient;
+    }
+
+    public void addUnAdditif (Additif unAdditif) {
+        if (lesProduits == null){
+            lesAdditifs = new ArrayList<Additif>();
+        }
+        lesAdditifs.add(unAdditif);
     }
 
     public ArrayList<Produit> getLesProduits() { return lesProduits; }
